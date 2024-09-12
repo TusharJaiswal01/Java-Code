95f0ae003a37d5098a91557fade725b10c9de930
tushar jaiswal <tusharjaiswaltj01@gmail.com>
2024-09-12 22:12:55 +0530

    array code

---
+++ MergeArrays.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MergeArrays {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Input first array
+        System.out.println("Enter the number of elements for the first array:");
+        int n1 = scanner.nextInt();
+        int[] array1 = new int[n1];
+        System.out.println("Enter " + n1 + " elements for the first array:");
+        for (int i = 0; i < n1; i++) {
+            array1[i] = scanner.nextInt();
+        }
+        
+        // Input second array
+        System.out.println("Enter the number of elements for the second array:");
+        int n2 = scanner.nextInt();
+        int[] array2 = new int[n2];
+        System.out.println("Enter " + n2 + " elements for the second array:");
+        for (int i = 0; i < n2; i++) {
+            array2[i] = scanner.nextInt();
+        }
+        
+        // Merge arrays
+        int[] mergedArray = mergeArrays(array1, array2);
+        
+        // Sort merged array
+        Arrays.sort(mergedArray);
+        
+        // Output result
+        System.out.println("Merged and sorted array:");
+        for (int num : mergedArray) {
+            System.out.print(num + " ");
+        }
+    }
+    
+    // Method to merge two arrays
+    public static int[] mergeArrays(int[] array1, int[] array2) {
+        int[] result = new int[array1.length + array2.length];
+        int i = 0;
+        for (int num : array1) {
+            result[i++] = num;
+        }
+        for (int num : array2) {
+            result[i++] = num;
+        }
+        return result;
+    }
+}
