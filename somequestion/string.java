4109eb3d7d87bbac715f3f315c82ab06f9a60d0f
tushar jaiswal <tusharjaiswaltj01@gmail.com>
2024-09-20 22:04:06 +0530

    string file change

--- somequestion/string.java
+++ somequestion/string.java
@@ -1,33 +1,18 @@
 
-//
 
-
-public class string {
-    public static void main(String[] args) {
-        String str = "Hello World";
-
-
-        // 1. length of string
-        System.out.println(str.length());
-
-        // 2. charAt
-        System.out.println(str.charAt(0));
-
-        // 3. substring
-        System.out.println(str.substring(0, 5));
-
-        // 4. indexOf
-        System.out.println(str.indexOf("World"));
-
-        // 5. replace
-        System.out.println(str.replace("World", "Java"));
-
-        // 6. toLowerCase
-        System.out.println(str.toLowerCase());
-
-        // 7. toUpperCase
-        System.out.println(str.toUpperCase());
-        System.out.println(str);
+class Ram {
+    public boolean isSubsequence(String s, String t) {
+        
+        int i=0;
+        int j=0;
+        
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        
+        return i==s.length();
     }
 }
-
