65afb5b1bf95f397e57f2e9966a3c17607e1f8b8
tushar jaiswal <tusharjaiswaltj01@gmail.com>
2024-09-21 23:12:35 +0530

    string code

--- somequestion/one.java
+++ somequestion/one.java
@@ -1,29 +1,20 @@
-package somequestion;
 
-public class one {
-    
-    public static void main(String[] args) {
-        // Create a new customer
-        Customer customer = new Customer("1", "Alice", "
-        // Create a new product
-        Product product = new Product("1", "Phone", 500.0);
-
-        // Create a new order
-        Order order = new Order(customer);
-        order.addProduct(product);
-
-        // Print the order details
-        System.out.println(order);
-
-        // Create a new customer
-        Customer customer2 = new Customer("2", "Bob", "
-
-        // Create a new product
-        Product product2 = new Product("2", "Laptop", 1000.0);
-
-
-        // Create a new order
-        Order order2 = new Order(customer2);
-        order2.addProduct(product2);
+class Solution {
+    public int maxArea(int[] height) {
         
+        int i=0;
+        int j=height.length-1;
+        int max=0;
+        
+        while(i<j){
+            max=Math.max(max,Math.min(height[i],height[j])*(j-i));
+            if(height[i]<height[j]){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        
+        return max;
+    }
 }
