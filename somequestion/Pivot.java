7f96641afc04c7f3d89981f5a42f59bfb81da311
Tushar Jaiswal <tusharjaiswaltj01@gmail.com>
2024-09-27 21:51:19 +0530

    pivot index code

---
+++ somequestion/Pivot.java
@@ -0,0 +1,30 @@
+public class Pivot {
+    public static void main(String[] args) {
+        int array[] = {1, 7, 3, 9, 1, 3, 7};
+        int totalSum = 0;
+        int leftSum = 0;
+
+  
+        for (int i = 0; i < array.length; i++) {
+            totalSum += array[i];
+        }
+
+       
+        for (int i = 0; i < array.length; i++) {
+           
+            int rightSum = totalSum - leftSum - array[i];
+
+          
+            if (leftSum == rightSum) {
+                System.out.println("Pivot found at index: " + i);
+                return; 
+            }
+
+            
+            leftSum += array[i];
+        }
+
+       
+        System.out.println("No pivot found where left sum equals right sum.");
+    }
+}
