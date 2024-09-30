22814ec2b6612ab8af8b60ab4129643687e75a56
Tushar Jaiswal <tusharjaiswaltj01@gmail.com>
2024-09-30 08:22:22 +0530

    constructor code

---
+++ oops/contructor.java
@@ -0,0 +1,43 @@
+class Animal {
+    String name;
+
+    Animal(String name) {
+        this.name = name;
+    }
+
+    void sound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    void sound() {
+        System.out.println("Bark");
+    }
+}
+
+class Cat extends Animal {
+    Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    void sound() {
+        System.out.println("Meow");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal myDog = new Dog("Buddy");
+        Animal myCat = new Cat("Whiskers");
+
+        myDog.sound();
+        myCat.sound();
+    }
+}
