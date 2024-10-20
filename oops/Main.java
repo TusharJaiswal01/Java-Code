71c7cebbd40a57732696a97381434ddcfa469e0c
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-10-20 20:01:33 +0530

    Update Main.java

--- oops/Main.java
+++ oops/Main.java
@@ -12,7 +12,7 @@ class Product {
     public Product(String id, String name, double price) {
         this.id = id;
         this.name = name;
-        this.price = price;
+        this.price = price;//pricee
     }
 
     public String getId() {
