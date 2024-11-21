7870193b8a80d67df9b96cc8d4447c918aee1bb2
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-11-21 22:01:21 +0530

    Update Main.java

--- oops/Main.java
+++ oops/Main.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javs.util.Map;
-// cclass representing a product  and other testings are done and hte name is the dont of the 
+// cclass representing a product  and other testings are done and hte name is the dont of 
 class Product {
     private String id;
     private String name;
