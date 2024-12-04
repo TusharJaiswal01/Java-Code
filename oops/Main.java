e032bdcb4a80cc17dcd692ae110dd311aad3fa34
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-04 07:57:24 +0530

    Update Main.java

--- oops/Main.java
+++ oops/Main.java
@@ -1,4 +1,4 @@
-//note: java is platform independent language because of byte code jvm 
+//note: java is platform independent language because of byte code jvm jav virtual machine 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
