0212d8bb6a53e6d701447fa86f3864e7c5a0af1e
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-07 09:20:34 +0530

    multithreading code

--- somequestion/some_concept.java
+++ somequestion/some_concept.java
@@ -2,3 +2,17 @@ public class MultiThreadExample extends Thread {
     public void run() {
         System.out.println("Thread is running...");
     }
+
+public class MultiTaskingExample implements Runnable {
+    private String task;
+
+    public MultiTaskingExample(String task) {
+        this.task = task;
+    }
+
+    public void run() {
+        System.out.println(task + " is running by " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MultiTaskingExample("
