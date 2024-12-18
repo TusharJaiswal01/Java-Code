e8e44d2b1a6a2fa799b07cd6ecb79c3cf9a4ef79
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-18 18:41:33 +0530

    Update advancejava2.java

--- advancejava2.java
+++ advancejava2.java
@@ -74,3 +74,17 @@ public class EnumExample {
         }
     }
 }
+
+
+
+import java.util.*;
+import java.util.stream.*;
+
+public class StreamsExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        numbers.stream()
+               .filter(n -> n % 2 == 0)
+               .forEach(System.out::println);
+    }
+}
