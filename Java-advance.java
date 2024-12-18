79922aec65bdcd5761a7b8d9c3e087c55187226c
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-18 18:37:40 +0530

    Update Java-advance.java

--- Java-advance.java
+++ Java-advance.java
@@ -66,3 +66,17 @@ public class FileWriteExample {
         writer.close();
     }
 }
+
+
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        Socket socket = serverSocket.accept();
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        System.out.println("Client says: " + in.readLine());
+    }
+}
