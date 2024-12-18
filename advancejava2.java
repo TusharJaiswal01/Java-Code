98edad151bbe9a7f050ecf17766b874a7ae358e2
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-18 18:40:59 +0530

    Update advancejava2.java

--- advancejava2.java
+++ advancejava2.java
@@ -44,3 +44,19 @@ public class GenericExample<T> {
         System.out.println(example.get());
     }
 }
+
+
+
+import java.util.concurrent.*;
+
+public class CallableExample {
+    public static void main(String[] args) throws Exception {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Integer> future = executor.submit(() -> {
+            Thread.sleep(1000);
+            return 42;
+        });
+        System.out.println("Result: " + future.get());
+        executor.shutdown();
+    }
+}
