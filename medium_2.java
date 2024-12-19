13b345a4a6bcb166eff1360233269dbd7798a45c
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-19 18:50:26 +0530

    Update medium_2.java

--- medium_2.java
+++ medium_2.java
@@ -14,3 +14,19 @@ public class FileReadWrite {
         reader.close();
     }
 }
+
+
+
+
+public class LinearSearch {
+    public static int search(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 7, 1, 8};
+        System.out.println(search(arr, 7)); // 2
+    }
+}
