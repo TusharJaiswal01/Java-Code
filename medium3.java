3edb5588244a1b140c733a61855742247b7dd8a0
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-19 18:59:13 +0530

    Update medium3.java

--- medium3.java
+++ medium3.java
@@ -159,3 +159,17 @@ public class PrimeNumber {
         System.out.println(isPrime(10)); // false
     }
 }
+
+
+
+
+
+
+public class ReverseString {
+    public static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(reverse("hello")); // "olleh"
+    }
+}
