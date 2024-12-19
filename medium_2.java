c238abf6ec188893a6a88399ecc457020a02bf94
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-19 18:52:09 +0530

    Update medium_2.java

--- medium_2.java
+++ medium_2.java
@@ -85,3 +85,16 @@ public class RemoveVowels {
         System.out.println(removeVowels("hello world")); // "hll wrld"
     }
 }
+
+
+
+
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr)); // [1, 2, 3, 5, 8]
+    }
+}
