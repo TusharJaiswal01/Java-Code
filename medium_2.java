02ada84569c7a945a7f97e2dc52f9f5ccc408e39
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-19 18:51:27 +0530

    Update medium_2.java

--- medium_2.java
+++ medium_2.java
@@ -1,4 +1,4 @@
-import java.io.*;
+7import java.io.*;
 
 public class FileReadWrite {
     public static void main(String[] args) throws IOException {
@@ -51,5 +51,26 @@ public class MatrixTranspose {
             }
             System.out.println();
         }
+    }
+}
+
+
+
+
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent")); // true
+        System.out.println(isAnagram("hello", "world")); // false
     }
 }
