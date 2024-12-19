3729f4b9fac6b7478e6deccff0034eefca150947
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-19 18:54:28 +0530

    Update medium3.java

--- medium3.java
+++ medium3.java
@@ -11,3 +11,21 @@ public class ArraySum {
         System.out.println(sumArray(arr)); // 15
     }
 }
+
+
+
+
+
+public class DigitCounter {
+    public static int countDigits(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num /= 10;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(countDigits(12345)); // 5
+    }
+}
