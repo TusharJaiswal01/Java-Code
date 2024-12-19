f796913aa551052a59f18fcc76aa516bd53669a3
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-19 18:59:39 +0530

    Update medium3.java

--- medium3.java
+++ medium3.java
@@ -173,3 +173,20 @@ public class ReverseString {
         System.out.println(reverse("hello")); // "olleh"
     }
 }
+
+
+
+public class ArmstrongNumber {
+    public static boolean isArmstrong(int num) {
+        int sum = 0, temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, 3);
+            temp /= 10;
+        }
+        return sum == num;
+    }
+    public static void main(String[] args) {
+        System.out.println(isArmstrong(153)); // true
+    }
+}
