9dd6b892c468e8c9fe04fe74116ff15d127188ba
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-19 18:54:08 +0530

    Update

---
+++ medium3.java
@@ -0,0 +1,13 @@
+public class ArraySum {
+    public static int sumArray(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(sumArray(arr)); // 15
+    }
+}
