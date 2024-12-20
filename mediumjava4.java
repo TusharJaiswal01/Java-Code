90cccb320432f662f732028fe7acdff8bab97c0b
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-20 22:43:43 +0530

    Update mediumjava4.java

--- mediumjava4.java
+++ mediumjava4.java
@@ -171,3 +171,28 @@ public class GCD {
         System.out.println("GCD of 54 and 24: " + gcd(54, 24));
     }
 }
+
+
+
+
+
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2}, {3, 4}};
+        int[][] b = {{2, 0}, {1, 2}};
+        int[][] result = new int[2][2];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        for (int[] row : result) {
+            System.out.println(java.util.Arrays.toString(row));
+        }
+    }
+}
