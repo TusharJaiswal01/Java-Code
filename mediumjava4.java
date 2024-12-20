c0798144def98ab4ae30203cb40166fef3ab5bdf
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-20 22:40:26 +0530

    Update mediumjava4.java

--- mediumjava4.java
+++ mediumjava4.java
@@ -16,3 +16,26 @@ public class UserInput {
         scanner.close();
     }
 }
+
+
+
+
+
+
+
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean isPrime = true;
+        
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        
+        System.out.println(num + " is " + (isPrime ? "prime" : "not prime"));
+    }
+}
