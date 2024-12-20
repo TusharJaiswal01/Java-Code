7835bd2403944616958f65feccb3670533c5374e
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-20 22:46:05 +0530

    Update medium5.java

--- medium5.java
+++ medium5.java
@@ -1,4 +1,10 @@
-public class Armstrong {
+public class WordCount {
+    public static void main(String[] args) {
+        String str = "Hello world, welcome to Java.";
+        int wordCount = str.split("\\s+").length;
+        System.out.println("Number of words: " + wordCount);
+    }
+}public class Armstrong {
     public static void main(String[] args) {
         int num = 153, sum = 0, temp = num;
 
