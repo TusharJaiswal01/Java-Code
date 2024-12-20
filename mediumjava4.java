acfd215320b508f08aa5c3725a57685b18f6951e
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-20 22:41:06 +0530

    Update mediumjava4.java

--- mediumjava4.java
+++ mediumjava4.java
@@ -49,3 +49,14 @@ public class ReverseString {
         System.out.println("Reversed string: " + reversed);
     }
 }
+
+public class Factorial {
+    public static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial of 5: " + factorial(5));
+    }
+}
