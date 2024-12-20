d2311915cb3385f8ae8dddb16c1fab6df9a8ca4c
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-20 22:48:08 +0530

    Update medium5.java

--- medium5.java
+++ medium5.java
@@ -80,3 +80,14 @@ public class FileRead {
         System.out.println(content);
     }
 }
+
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        a = a + b;  
+        b = a - b;  
+        a = a - b;  
+        System.out.println("a: " + a + ", b: " + b);
+    }
+}
