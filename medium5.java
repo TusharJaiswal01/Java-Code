ad087f7e77cea5e8cbd66b8b55a4cfc74d0ebe05
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-20 22:50:34 +0530

    Update medium5.java

--- medium5.java
+++ medium5.java
@@ -91,3 +91,15 @@ public class SwapNumbers {
         System.out.println("a: " + a + ", b: " + b);
     }
 }
+
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 12345, reversed = 0;
+        while (num != 0) {
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+        }
+        System.out.println("Reversed number: " + reversed);
+    }
+}
