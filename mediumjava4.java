8ebda92ebc26dae01b71997d56da44a9e784073c
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-20 22:44:04 +0530

    Update mediumjava4.java

--- mediumjava4.java
+++ mediumjava4.java
@@ -196,3 +196,19 @@ public class MatrixMultiplication {
         }
     }
 }
+
+
+
+
+public class VowelCount {
+    public static void main(String[] args) {
+        String str = "hello world";
+        int count = 0;
+
+        for (char c : str.toLowerCase().toCharArray()) {
+            if ("aeiou".indexOf(c) != -1) count++;
+        }
+
+        System.out.println("Number of vowels: " + count);
+    }
+}
