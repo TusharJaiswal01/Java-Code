2336a3eb39575524ef330f869ed19432ce2520c4
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-20 22:42:25 +0530

    Update mediumjava4.java

--- mediumjava4.java
+++ mediumjava4.java
@@ -110,3 +110,21 @@ public class BubbleSort {
         System.out.println("Sorted array: " + java.util.Arrays.toString(arr));
     }
 }
+
+
+
+
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10, a = 0, b = 1;
+        System.out.print("Fibonacci Series: " + a + ", " + b);
+
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(", " + next);
+            a = b;
+            b = next;
+        }
+    }
+}
