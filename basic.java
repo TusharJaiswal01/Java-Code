2541fcad4710eca8f4fcc4c6686899d21be4eeba
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-21 19:08:20 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -76,3 +76,11 @@ public class StringToInt {
         System.out.println(num + 1);
     }
 }
+
+public class StringToInt {
+    public static void main(String[] args) {
+        String str = "123";
+        int num = Integer.parseInt(str);
+        System.out.println(num + 1);
+    }
+}
