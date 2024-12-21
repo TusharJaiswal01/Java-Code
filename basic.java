c6be6db85869e8b65a44d2f47dc7b1ba2197a902
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-21 19:13:48 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -130,3 +130,15 @@ public class MultiplicationTable {
         }
     }
 }
+
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num = 1234, sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
