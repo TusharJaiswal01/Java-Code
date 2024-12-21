69cf0a29b73114822b7ebe20f0bb368dcb7da3c7
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-21 19:09:35 +0530

    Update array.java

--- array.java
+++ array.java
@@ -5,3 +5,12 @@ public class MaxElement {
         System.out.println(max);
     }
 }
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
