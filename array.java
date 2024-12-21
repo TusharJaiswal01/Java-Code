e27054aa35095a2fc11c863302e5942f0e395622
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-21 19:10:23 +0530

    Update array.java

--- array.java
+++ array.java
@@ -37,3 +37,14 @@ public class FindDuplicates {
         }
     }
 }
+
+
+
+import java.util.Arrays;
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 1};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
