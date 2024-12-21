33983dc1214598e5fa431136afeb0ef992f8dbd8
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-21 19:05:09 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -42,3 +42,11 @@ public class PrimeCheck {
         System.out.println(isPrime ? "Prime" : "Not Prime");
     }
 }
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Java";
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println(reversed);
+    }
+}
