dffbf1f8014b2c081a51089412f3c27f5d868e11
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-21 19:13:12 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -119,3 +119,14 @@ public class NumberPyramid {
         }
     }
 }
+
+
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int num = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + num * i);
+        }
+    }
+}
