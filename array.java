e91725ef266df72401f650250d10cc882a71263e
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-21 19:09:52 +0530

    Update array.java

--- array.java
+++ array.java
@@ -14,3 +14,13 @@ public class ReverseArray {
         }
     }
 }
+
+
+import java.util.Arrays;
+public class ArrayEquality {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {1, 2, 3};
+        System.out.println(Arrays.equals(arr1, arr2));
+    }
+}
