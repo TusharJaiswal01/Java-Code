df13913d674b7d4a155966cf3c77d1226e931b08
TusharJaiswal <143197150+TusharJaiswal01@users.noreply.github.com>
2024-12-21 19:06:18 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -67,3 +67,12 @@ public class VowelCount {
         System.out.println("Vowels: " + count);
     }
 }
+
+
+public class StringToInt {
+    public static void main(String[] args) {
+        String str = "123";
+        int num = Integer.parseInt(str);
+        System.out.println(num + 1);
+    }
+}
